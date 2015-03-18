379c4270a61de9ae17845f3e3526fa408980b8a5
Themis Mavridis <themismavridis@gmai.com>
2015-03-18 14:45:33 +0200

    added licence

--- src/main/java/com/thesmartweb/lshrank/LDAcall.java
+++ src/main/java/com/thesmartweb/lshrank/LDAcall.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2015 themis.
+ * Copyright 2015 Themistoklis Mavridis <themis.mavridis@issel.ee.auth.gr>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
